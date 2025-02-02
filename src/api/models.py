e30b4912fe6463f4d2c75470b2f345c2ec2921e9
lluisespert <lluisespertc@gmail.com>
2025-02-02 15:39:36 +0000

    Prueba 1

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    phone = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.id} - {self.email}>'
+        #        <User: 1 - admin@example>
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'email': self.email,
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'phone': self.phone}
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=False, nullable=False)
+    description = db.Column(db.String(), unique=False, nullable=False)
+    body = db.Column(db.String(), unique=False, nullable=True)
+    date = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow)  # TODO: default !?
+    image_url = db.Column(db.String(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('posts_to', lazy='select'))
+
+    def __repr__(self):
+        return f'<Post: {self.id} - {self.title}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'body': self.body,
+                'date': self.date,
+                'image_url': self.image_url,
+                'user_id': self.user_id}
+
+class Medias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'), unique=False, nullable=False)
+    url = db.Column(db.String(), unique=True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), unique=True)
+    post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('media_to'), lazy='select')
+
+
+class BillItems(db.Model):
+    __tablename__ = 'bill_items'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), unique=False, nullable=False)
+    description = db.Column(db.String(), unique=False, nullable=False)
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_to = db.relationship('Users', foreign_keys=[follower_id], backref=db.backref('followers_to', lazy='select'))
+    following_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    following_to = db.relationship('Users', foreign_keys=[following_id], backref=db.backref('followings_to', lazy='select'))
+
+    def __repr__(self):
+        return f'<Follower: {self.follower_id} - Following {self.following_id}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,131 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from api.models import db, Users, Posts
+import requests
 
 api = Blueprint('api', __name__)
-
-# Allow CORS requests to this API
-CORS(api)
+CORS(api)  
 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
+    response_body = {}
+    response_body['message'] = "Hello! I'm a message that came from the backend"
+    return response_body, 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+@api.route('/users', methods=['GET'])
+def users():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Users)).scalars()
+        result = [ row.serialize() for row in rows ]
+        response_body['message'] = 'Listado de Usuarios'
+        response_body['results'] = result
+        return response_body, 200
+
+
+
+@api.route('/posts', methods=['GET', 'POST'])
+def posts():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Posts)).scalars()
+
+        result = [ row.serialize() for row in rows ]
+        response_body['message'] = f'Listado de todas las publicaciones (de todos los usuarios)'
+        response_body['results'] = result
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        row = Posts(title=data.get('title'),
+                    description=data.get('description'),
+                    body=data.get('body', 'body por defecto cuando hay un error'),
+                    image_url=data['image_url'],
+                    user_id=data['user_id'])
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = f'El post ha sido publicado correctamente'
+        response_body['results'] = row.serialize()
+        return response_body, 200
+
+
+@api.route('/posts/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def post(id):
+    response_body = {}
+    row = db.session.execute(db.select(Posts).where(Posts.id == id)).scalar()
+    
+    if not row:
+        response_body['message'] =  f'La publicación con id: {id} no existe en nuestro registos'
+        return response_body, 400
+    if request.method == 'GET':
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'Respuesta desde el {request.method} para el id: {id}'
+        return response_body, 200
+    if request.method == 'PUT':
+        data = request.json
+        row.title = data['title']
+        row.description = data['description']
+        row.body = data['body']
+        row.image_url = data['image_url']
+        row.user_id = data['user_id']
+        db.session.commit()
+        response_body['message'] = f'Respuesta desde el {request.method} para el id: {id}'
+        response_body['results'] = row.serialize()
+        return response_body, 200
+    if request.method == 'DELETE':
+        db.session.delete(row)
+        db.session.commit()
+        response_body['message'] = f'Respuesta desde el {request.method} para el id: {id}'
+        return response_body, 200
+
+
+@api.route('/users/<int:user_id>/posts', methods=['GET'])
+def users_posts(user_id):
+    response_body = {}
+   
+    response_body['message'] = 'todos las publicaciones de un usuario'
+    return response_body, 200
+
+
+@api.route('/posts/<int:post_id>/comments', methods=['GET'])
+def post_comments(post_id):
+    response_body = {}
+
+    response_body['message'] = 'todos los comenarios de una publicación'
+    return response_body, 200
+
+
+@api.route('/users/<int:user_id>/comments', methods=['GET'])
+def user_comments():
+    response_body = {}
+    response_body['message'] = 'todos los comenarios de una usuario'
+    return response_body, 200
+
+
+@api.route('/jph-users', methods=['GET'])
+def jph_users():
+    response_body = {}
+    url = 'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(url)
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        response_body['results'] = data
+        return response_body, 200
+    response_body['message'] = 'Algún error'
+    return response_body, 400
+
+
+@api.route('/temp')
+def temp():
+    response_body = {}
+    url = 'https://swapi.tech/api/people/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        response_body['results'] = data
+        return response_body, 200
